45b7d83c70312ccabf9eeffa9bef9607aaa04ecd
kouhei-github <kohei0801nagamatsu@gmail.com>
2023-11-04 16:21:13 +0900

    パッケージの依存関係の更新と useGetArticle フックの実装
    swr` パッケージが `package.json` の依存関係として追加され、`page.tsx` の Home コンポーネントに実装された。useGetArticle` フックは記事データを ID で取得し、読み込みとエラー状態を処理するために使用される。また、`yarn.lock` ファイルも新しいパッケージ構成とバージョン更新を反映するために更新された。

--- nextjs/app/test/page.tsx
+++ nextjs/app/test/page.tsx
@@ -3,15 +3,19 @@ import markdownHtml from 'zenn-markdown-html';
 import Script from "next/script";
 import {useEffect} from "react";
 import 'zenn-content-css';
+import {useGetArticle} from "@/utils/api/article_fetch";
 
 
 export default function Home() {
 
-  const markdownText = ``
-  const html = markdownHtml(markdownText);
+  const {article, isError, isLoading} = useGetArticle(2)
   useEffect(() => {
     import('zenn-embed-elements');
   }, []);
+  if (isError) return <div>load is Failed</div>
+  if (isLoading) return <div>Loadin ...</div>
+  if (typeof article === "undefined") return <div>load is Failed</div>
+  const html = markdownHtml(article.description);
   return (
     <>
       <Script src={"https://embed.zenn.studio/js/listen-embed-event.js"}/>

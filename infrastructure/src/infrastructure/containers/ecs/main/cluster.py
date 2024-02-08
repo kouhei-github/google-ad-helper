90239c3f7ee8ccb79b9889f4ced1ab74596e21af
kouhei-github <kohei0801nagamatsu@gmail.com>
2024-02-08 17:51:36 +0900

    削除

--- infrastructure/src/infrastructure/containers/ecs/main/cluster.py
+++
@@ -1,9 +0,0 @@
-import pulumi_aws as aws
-
-
-def create(cluster_config: dict):
-    return aws.ecs.Cluster(
-        "ecs-cluster-" + cluster_config['name'],
-        name="cluster-" + cluster_config['name'],
-        tags={"Name": "Author", "Author": "Pulumi"},
-    )
